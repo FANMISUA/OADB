e2a51a63a1a70f438a3bf1178c899c9b7aa35398
FANMISUA <FANMISUA@users.noreply.github.com>
2022-12-13 04:07:04 -0600

    update path issue in load data.sql

--- PostgreSQL/loadData.sql
+++ PostgreSQL/loadData.sql
@@ -1,4 +1,4 @@
-\copy Chemical FROM 'C:\temp\data\test_oadb\FinalChemicalTable.csv' with delimiter as ',' CSV HEADER;
-\copy Reference FROM 'C:\temp\data\test_oadb\FinalReferenceTable.csv' with delimiter as ',' CSV HEADER;
-\copy Assay FROM 'C:\temp\data\test_oadb\FinalAssayTable.csv' with delimiter as ',' CSV HEADER;
-\copy Data FROM 'C:\temp\data\test_oadb\FinalDataTable.csv' with delimiter as ',' CSV HEADER;
+\copy Chemical FROM 'FinalChemicalTable.csv' with delimiter as ',' CSV HEADER;
+\copy Reference FROM 'FinalReferenceTable.csv' with delimiter as ',' CSV HEADER;
+\copy Assay FROM 'FinalAssayTable.csv' with delimiter as ',' CSV HEADER;
+\copy Data FROM 'FinalDataTable.csv' with delimiter as ',' CSV HEADER;
